eca09c3924a41c1143273f03687994f1f8c1b5e7
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-03-09 19:50:09 +0300

    Add newline at end of file

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 void main() {
     faStr1("asf ahf GdSwwaA sa8sd 8a 8 a asd89 98087");
     faStr1("9");
-    faStr2("Agop AdsaA Bs0s L bBDS 9 092 a");
+    faStr2("Agop AdsaA Bs0s L bBDS 9 092 c");
     faStr3("Hello_World!");
     faStr3(" d sa sda fsad fsads");
 }
