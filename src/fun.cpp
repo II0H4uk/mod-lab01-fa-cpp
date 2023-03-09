dab64c4bfdbba20f48832e8414b3227c9f9c4688
Андрей Урмаев <urmaer72@gmail.com>
2023-03-09 18:37:41 +0300

    first commit

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,68 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <Math.h>
 
-unsigned int faStr1(const char *str) {
-    return 1;
+unsigned int faStr1(const char* str) {
+    bool inWord = false, isGoodWord = true;
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && !inWord)
+            inWord = true;
+        if (str[i] >= '0' && str[i] <= '9' && inWord && isGoodWord)
+            isGoodWord = false;
+        if (str[i] == ' ' && inWord) {
+            if (isGoodWord)
+                count++;
+            inWord = false;
+            isGoodWord = true;
+        }
+    }
+    if (inWord && isGoodWord)
+        count++;
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    bool inWord = false, isGoodWord = true;
+    int count = 0, firstLetterGood = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && !inWord)
+            inWord = true;
+        if (str[i] >= 'A' && str[i] <= 'Z' && inWord && firstLetterGood == 0) {
+            firstLetterGood = 1;
+            continue;
+        }
+        if (str[i] >= 33 && str[i] <= 64 && inWord && isGoodWord)
+            isGoodWord = false;
+        if (str[i] >= 'A' && str[i] <= 'Z' && firstLetterGood == 1)
+            firstLetterGood = 2;
+        if (str[i] == ' ' && inWord) {
+            if (isGoodWord && firstLetterGood == 1)
+                count++;
+            inWord = false;
+            isGoodWord = true;
+            firstLetterGood = 0;
+        }
+    }
+    if (inWord && isGoodWord && firstLetterGood == 1)
+        count++;
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    bool lastWasLetter = false;
+    int wordsCount = 0, lettersCount = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            lastWasLetter = true;
+            lettersCount++;
+        }
+        if (str[i] == ' ' && lastWasLetter) {
+            wordsCount++;
+            lastWasLetter = false;
+        }
+    }
+    if (lastWasLetter)
+        wordsCount++;
+    return round(lettersCount / wordsCount);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
-int main() {
-    return 0;
+void main() {
+    std::cout << faStr1("asf ahf GdSwwaA sa8sd 8a 8 a asd89 98087");
+    std::cout << faStr1("9");
+    std::cout << faStr2("Agop AdsaA Bs0s L bBDS 9 092 a");
+    std::cout << faStr3("Hello_World!");
+    std::cout << faStr3(" d sa sda fsad fsads");
 }
