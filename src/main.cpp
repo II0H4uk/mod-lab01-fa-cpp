e9812987987a514b8e386fcd99da7fe50fddfcdd
II0H4uk <126766004+II0H4uk@users.noreply.github.com>
2023-03-09 19:42:42 +0300

    fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
 
 void main() {
-    std::cout << faStr1("asf ahf GdSwwaA sa8sd 8a 8 a asd89 98087");
-    std::cout << faStr1("9");
-    std::cout << faStr2("Agop AdsaA Bs0s L bBDS 9 092 a");
-    std::cout << faStr3("Hello_World!");
-    std::cout << faStr3(" d sa sda fsad fsads");
+    faStr1("asf ahf GdSwwaA sa8sd 8a 8 a asd89 98087");
+    faStr1("9");
+    faStr2("Agop AdsaA Bs0s L bBDS 9 092 a");
+    faStr3("Hello_World!");
+    faStr3(" d sa sda fsad fsads");
 }
